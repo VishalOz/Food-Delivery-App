b610642041b6caf6b647c4b53f884dfb4dbd9327
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-30 10:30:49 +0530

    Completed the restaurant class

--- FoodDeliveryApp.java
+++ FoodDeliveryApp.java
@@ -1,5 +1,12 @@
 public class FoodDeliveryApp {
     public static void main(String[] args) {
 
+        Restaurant restaurant1 = new Restaurant("Pizza Butt");
+        MenuItem menu1
+        System.out.println("=====================================================");
+        System.out.println("|                     CUBER Eats                     |");
+        System.out.println("=====================================================");
     }
+
+
 }

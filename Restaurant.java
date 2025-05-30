46c070dbb463ac5f7c1ab6b1d7e2deeb6e0cdae3
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-30 09:38:44 +0530

    Restauran, Agent class done

--- Restaurant.java
+++ Restaurant.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
+    private String name;
+    private List<MenuItem> menu;
+
+    public Restaurant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
